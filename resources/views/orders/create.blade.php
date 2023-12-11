86267ace4cfdb93d42e8adff3e058945b1cf0019
“ieie0727” <you.inoue.727@gmail.com>
2023-12-11 12:29:32 +0900

    行数追加＆微修正

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -23,7 +23,7 @@
       <form method="POST" action="{{route('orders.store')}}">
         @csrf
         <div class="card-body">
-          <table class="table">
+          <table id="orderTableBody" class="table">
             <thead>
               <tr>
                 <th>No.</th>
@@ -37,7 +37,7 @@
               @for ($i = 1; $i <= 5; $i++) <tr>
                 <td>{{ $i }}</td>
                 <td>
-                  <select class="id" id="id-{{$i}}" name="order_items[{{$i}}][id]">
+                  <select class="id" name="order_items[{{$i}}][id]">
                     <option value="" selected disabled></option>
                     @foreach ($items as $item)
                     <option value="{{$item->id}}">{{$item->name}}（{{$item->artist}}）</option>
@@ -45,21 +45,19 @@
                   </select>
                 </td>
                 <td>
-                  <input type="number" class="price" id="price-{{$i}}" name="order_items[{{$i}}][price]" value="0"
-                    readonly>
+                  <input type="number" class="price" name="order_items[{{$i}}][price]" value="0" readonly>
                 </td>
                 <td>
-                  <input type="number" class="quantity" id="quantity-{{$i}}" name="order_items[{{$i}}][quantity]"
-                    value="0" min="0">
+                  <input type="number" class="quantity" name="order_items[{{$i}}][quantity]" value="0" min="0">
                 </td>
                 <td>
-                  <input type="number" class="sub-total" id="sub-total-{{$i}}" name="order_items[{{$i}}][sub_total]"
-                    value="0">
+                  <input type="number" class="sub-total" name="order_items[{{$i}}][sub_total]" value="0">
                 </td>
                 </tr>
                 @endfor
             </tbody>
           </table>
+          <button type="button" id="addRow" class="btn btn-secondary mb-3"><b>＋</b></button>
           <div class="card-footer">
             <div>
               <label for="total-amount">合計：</label>
@@ -83,7 +81,8 @@
 
 @section('css')
 <style>
-  .total-amount {
+  #addRow {
+    margin: 0 0 0 auto;
     text-align: right;
   }
 </style>
@@ -92,11 +91,42 @@
 
 @section('js')
 <script>
-  /*--------------------------------
-  //商品が選択されたら、単価を表示する
-  --------------------------------*/
+  /**
+   * 
+    */
   $(document).ready(()=>{
-    $('.id').on('input',function(){
+    $('#addRow').on('click',function(){
+      //現在の行数を取得
+      let rowCount =$('#orderTableBody tr').length -1; //-1はheader分を調整
+
+      //最大行数を超えていなければ新しい行を挿入
+      const maxLength =20;
+      if(rowCount<maxLength){
+        let i =Number(rowCount)+1;
+        let newRow =`<tr>`+
+                `<td>${i}</td>`+
+                `<td><select class="id" name="order_items[${i}][id]">`+
+                    `<option value="" selected disabled></option>`+
+                    `@foreach ($items as $item)`+
+                    `<option value="{{$item->id}}">{{$item->name}}（{{$item->artist}}）</option>`+
+                    `@endforeach`+
+                  `</select></td>`+
+                `<td><input type="number" class="price" name="order_items[${i}][price]" value="0" readonly></td>`+
+                `<td><input type="number" class="quantity" name="order_items[${i}][quantity]" value="0" min="0"></td>`+
+                `<td><input type="number" class="sub-total" name="order_items[${i}][sub_total]" value="0"></td>`+
+                `</tr>`;
+        $('#orderTableBody').append(newRow);
+      }else{
+        alert("一括注文は20点までです。");
+      }
+    })
+  })
+
+  /**
+   * 商品が選択されたら、単価を表示して、小計・合計を更新 
+  */
+  $(document).ready(()=>{
+    $(document).on('input','.id',function(){
       let thisElm =$(this);
       let thisRow =thisElm.closest('tr');
       let itemId =thisElm.val();
@@ -109,44 +139,47 @@
 
           success:function(response){
             thisRow.find('.price').val(response.price);
+            updateTotal(thisElm);
           },
           error:function(error){
             console.log('Error',error);
           }
         });
       }
-      
     });
   });
 
-  
-  /*------------------------------------
-  //個数を入力したら小計と合計が更新される
-  ------------------------------------*/
-  
+  //個数が入力されたら小計・合計を更新
   $(document).ready(()=>{
-    $('.quantity').on('input',function(){
+    $(document).on('input','.quantity',function(){
       let thisElm =$(this);
-      let thisRow =thisElm.closest('tr');
+      updateTotal(thisElm);
+    });
+  });
 
-      //小計の計算
-      let quantity =thisElm.val();
-      let price =thisRow.find('.price').val();
-      //console.log("個数："+quantity, "値段："+price);
-      let subTotal =quantity * price;
-      thisRow.find('.sub-total').val(subTotal);
 
-      
-      //合計の計算
-      let totalAmount =0;
+  /*-------------------------------
+  小計、合計のアップデート
+  -------------------------------*/
+  function updateTotal(elm){
+    //定義
+    let thisRow =elm.closest('tr');
+    let price =thisRow.find('.price').val();
+    let quantity =thisRow.find('.quantity').val();
+
+    //小計の計算
+    //console.log("個数："+quantity, "値段："+price);
+    let subTotal =quantity * price;
+    thisRow.find('.sub-total').val(subTotal);
+
+    //合計の計算
+    let totalAmount =0;
       $('.sub-total').each(function () {
                 totalAmount += Number($(this).val());
       });
       $('.total-amount').val(totalAmount);
-      
-    });
-  });
-  
+  }
+
 
 </script>
 @stop
