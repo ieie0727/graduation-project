ec485fcb7e9b34bf755d9870dd7064b94abe9aa9
“ieie0727” <you.inoue.727@gmail.com>
2024-03-26 10:35:01 +0900

    削除済アイテム

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -19,7 +19,7 @@ class Order extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     public function company()
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -17,6 +17,6 @@ class OrderItem extends Model
 
     public function item()
     {
-        return $this->belongsTo(Item::class);
+        return $this->belongsTo(Item::class)->withTrashed();
     }
 }
